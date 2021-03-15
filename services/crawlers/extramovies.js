be11ca4c750ed950b7dbafcb377a5e94a9cae50b
mnsrulz <manishrawat4u@gmail.com>
2021-03-14 21:14:10 -0400

    Update extramovies.js

--- services/crawlers/extramovies.js
+++ services/crawlers/extramovies.js
@@ -1,4 +1,4 @@
-const EXTRAMOVIES_API_BASE_URI = 'https://extramovies.world';
+const EXTRAMOVIES_API_BASE_URI = 'https://extramovies.team';
 const MEDIA_COLLECTION = "media_catalog";
 const axios = require('axios');
 const IMDBScraper = require('imdb-scrapper/index')
